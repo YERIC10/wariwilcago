973dae0a619eecf291adb3d2e65dc1247da33d24
Yeric10 <73600753@continental.edu.pe>
2021-11-25 12:45:11 -0500

    fragment home

--- app/src/main/java/com/example/wariwilca_go/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/wariwilca_go/ui/home/HomeFragment.java
@@ -1,9 +1,13 @@
 package com.example.wariwilca_go.ui.home;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +15,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.wariwilca_go.MainActivity;
 import com.example.wariwilca_go.R;
 import com.example.wariwilca_go.databinding.FragmentHomeBinding;
 import com.pierfrancescosoffritti.androidyoutubeplayer.player.YouTubePlayer;
@@ -18,21 +23,28 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.player.YouTubePlayerView;
 import com.pierfrancescosoffritti.androidyoutubeplayer.player.listeners.AbstractYouTubePlayerListener;
 import com.pierfrancescosoffritti.androidyoutubeplayer.player.listeners.YouTubePlayerInitListener;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     YouTubePlayerView video;
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
 
+    Button btnaccedercuenta;
+    private com.example.wariwilca_go.databinding.FragmentHomeBinding binding1;
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         binding = FragmentHomeBinding.inflate(inflater, container, false);
+
         View root = binding.getRoot();
 
+        btnaccedercuenta = binding.btnHomeAcceso;
         video = binding.videoFraghome;
         getLifecycle().addObserver(video);
         reproducirVideo();
+
+        btnaccedercuenta.setOnClickListener(this);
 
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -64,5 +76,32 @@ public class HomeFragment extends Fragment {
                 });
             }
         }, true);
+    }
+
+    public void mostrarDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        LayoutInflater inflater = getLayoutInflater();
+        builder.setIcon(R.drawable.ic_home_bienvenida);
+        builder.setTitle("!Te Damos la Bienvenida¡");
+        builder.setMessage("Accede a tu cuenta para guardar tus puntuaciones")
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        })
+                .setPositiveButton("Acceso", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getContext(), "ACCEDIENDO", Toast.LENGTH_SHORT).show();
+            }
+        })
+        .setCancelable(false)
+        .show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        mostrarDialog();
     }
 }
