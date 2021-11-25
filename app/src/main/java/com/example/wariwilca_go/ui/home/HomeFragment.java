786b71f1da37bcdba2164cce38f1fc3e4992ef10
Yeric10 <73600753@continental.edu.pe>
2021-11-25 00:48:50 -0500

    fragment home

--- app/src/main/java/com/example/wariwilca_go/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/wariwilca_go/ui/home/HomeFragment.java
@@ -11,28 +11,36 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.wariwilca_go.R;
 import com.example.wariwilca_go.databinding.FragmentHomeBinding;
+import com.pierfrancescosoffritti.androidyoutubeplayer.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.player.YouTubePlayerView;
+import com.pierfrancescosoffritti.androidyoutubeplayer.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.player.listeners.YouTubePlayerInitListener;
 
 public class HomeFragment extends Fragment {
 
+    YouTubePlayerView video;
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        //final TextView textView = binding.textHome;
+        video = binding.videoFraghome;
+        getLifecycle().addObserver(video);
+        reproducirVideo();
+
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
 
             }
         });
+
         return root;
     }
 
@@ -40,5 +48,21 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void reproducirVideo() {
+
+        video.initialize(new YouTubePlayerInitListener() {
+            @Override
+            public void onInitSuccess(@NonNull final YouTubePlayer initializeYouTubePlayer) {
+                initializeYouTubePlayer.addListener(new AbstractYouTubePlayerListener() {
+                    @Override
+                    public void onReady() {
+                        String videoURL = "igt22WpLuF0";
+                        initializeYouTubePlayer.loadVideo(videoURL, 0);
+                    }
+                });
+            }
+        }, true);
     }
 }
